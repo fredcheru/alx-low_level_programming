d8fb9e0451f292c82ef93d1aa8c1ca874d4ceed9
fredcheru <fredcheru10@gmail.com>
2023-02-19 21:26:36 -0800

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - main command
+ * Return: is 0
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 'a' ; i <= 'z' ; i++)
+		if (i != 'e' && i != 'q')
+			putchar(i);
+	putchar('\n');
+	return (0);
+}
