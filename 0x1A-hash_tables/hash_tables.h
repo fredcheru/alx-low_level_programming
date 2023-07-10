f191600ba42171cfc756a4837993e5b2e95a5512
fredcheru <fredcheru10@gmail.com>
2023-07-09 19:40:16 -0700

    update 2

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -1,6 +1,9 @@
 #ifndef HASH_TABLES_H
 #define HASH_TABLES_H
 
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 
 /**
  * struct hash_node_s - Node of a hash table
@@ -12,9 +15,9 @@
  */
 typedef struct hash_node_s
 {
-	 char *key;
-	 char *value;
-	 struct hash_node_s *next;
+	char *key;
+	char *value;
+	struct hash_node_s *next;
 } hash_node_t;
 
 /**
@@ -27,38 +30,16 @@ typedef struct hash_node_s
  */
 typedef struct hash_table_s
 {
-	 unsigned long int size;
-	 hash_node_t **array;
+	unsigned long int size;
+	hash_node_t **array;
 } hash_table_t;
 
-
-/* Function Prototypes */
-/*=====================*/
-
-/* 0-hash_table_create.c */
 hash_table_t *hash_table_create(unsigned long int size);
-
-/* 1-djb2.c */
 unsigned long int hash_djb2(const unsigned char *str);
-
-/* 2-key_index.c */
 unsigned long int key_index(const unsigned char *key, unsigned long int size);
-
-/* 3-hash_table_set.c */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value);
-hash_node_t *key_exists(hash_node_t *head, const char *key);
-void update_node_value(hash_node_t *node, const char *value);
-hash_node_t *hash_node_create(const char *key, const char *value);
-
-/* 4-hash_table_get.c */
 char *hash_table_get(const hash_table_t *ht, const char *key);
-
-/* 5-hash_table_print.c */
 void hash_table_print(const hash_table_t *ht);
-
-/* 6-hash_table_delete.c */
 void hash_table_delete(hash_table_t *ht);
-void free_node_list(hash_node_t *head);
 
-
-#endif /* HASH_TABLES_H */
+#endif
